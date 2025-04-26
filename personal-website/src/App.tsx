cff7e48c72ec422010b03714f516f06645d325ba
Matt Smith <mattsmith1652@gmail.com>
2025-04-26 11:14:10 -0400

    fix coloring

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -2,18 +2,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home.tsx";
 import NavBar from "./components/nav-bar.tsx";
+import About from "./pages/about.tsx";
+import Resume from "./pages/resume.tsx";
 
 function App() {
 
   return (
-  <BrowserRouter>
+  <div className="bg-gray-700">
     <NavBar />
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/about" element={<Home />}/>
-      <Route path="/resume" element={<Home />}/>
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/resume" element={<Resume />}/>
+      </Routes>
+    </BrowserRouter>
+  </div>
   )
 }
 
--- personal-website/src/components/nav-bar.tsx
+++ personal-website/src/components/nav-bar.tsx
@@ -2,14 +2,15 @@ import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
     return (
-        <nav className="bg-white border-gray-200">
+        <nav className="bg-gray-700 border-gray-200">
             <div className="max-w-screen-xl flex flex-wrap items-center">
                 <NavLink to="/" className="flex items-center space-x-3 rtl:space-x-reverse"></NavLink>
                     <img src="./assets/leaf.png" className="h-8"/>
             </div>
             <div className="hidden w-full md:block md:w-auto">
-                <NavLink to="/about" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</NavLink>
-                <NavLink to="/resume" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Resume</NavLink>
+                <NavLink to="/about" className="block py-2 px-3 text-white rounded-sm hover:bg-green-100 md:hover:bg-transparent md:border-0 md:hover:text-green-100 md:p-0">About</NavLink>
+
+                <NavLink to="/resume" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-green-100 md:hover:bg-transparent md:border-0 md:hover:text-green-100 md:p-0">Resume</NavLink>
             </div>
         </nav>
     )
