31de54f9630e2df84c5073cf786cc0bfced1b083
Matt Smith <mattsmith1652@gmail.com>
2025-05-02 10:52:37 -0400

    style changes

--- personal-website/src/pages/Media.tsx
+++ personal-website/src/pages/Media.tsx
@@ -4,30 +4,29 @@ export default function Media() {
             <h1 className="mt-16 text-5xl text-center text-white mb-16">Featured Media</h1>
             <div className="flex flex-row justify-between items-center shadow-2xl shadow-black/50 mb-10 overflow-hidden rounded-2xl">
                 <div>
-                    <a href="https://www.gvsu.edu/careers/module-spotlight-view.htm?entryId=DD1C1E37-E12F-F7E4-AA1B3B48AA338371&siteModuleId=2C5D039C-CC9F-844D-585C9F0D2D785523" className="">
+                    <a href="https://www.gvsu.edu/careers/module-spotlight-view.htm?entryId=DD1C1E37-E12F-F7E4-AA1B3B48AA338371&siteModuleId=2C5D039C-CC9F-844D-585C9F0D2D785523">
                         <img src="/me.jpg" className='h-auto w-100' />
                     </a>
 
                 </div>
-                <div className="justify-items-center">
+                <div className="justify-items-center flex-grow">
                     <a href="https://www.gvsu.edu/careers/module-spotlight-view.htm?entryId=DD1C1E37-E12F-F7E4-AA1B3B48AA338371&siteModuleId=2C5D039C-CC9F-844D-585C9F0D2D785523">
-                        <h2 className="text-white m-2 font-semibold text-2xl">GVSU Adult Learner Profile</h2>
+                        <h2 className="text-white m-2 font-semibold text-2xl ">GVSU Adult Learner Profile</h2>
                     </a>
                     <p className="leading-relaxed text-lg text-white m-6 max-w-120">Read about my decision to return to school following my nursing career.</p>
-                    <a href="https://www.gvsu.edu/careers/module-spotlight-view.htm?entryId=DD1C1E37-E12F-F7E4-AA1B3B48AA338371&siteModuleId=2C5D039C-CC9F-844D-585C9F0D2D785523" className="bg-white py-3 px-4 rounded-xl hover:bg-neutral-700 hover:border-neutral-800 focus:outline-2 focus:outline-offset-1 focus:outline-neutral-900 shadow-black/30 shadow-lg">
+                    <a href="https://www.gvsu.edu/careers/module-spotlight-view.htm?entryId=DD1C1E37-E12F-F7E4-AA1B3B48AA338371&siteModuleId=2C5D039C-CC9F-844D-585C9F0D2D785523" className="bg-white py-3 px-4 rounded-xl hover:bg-neutral-200 duration-300 transform hover:scale-110 shadow-black/70 shadow-lg">
                         <span className="text-lime-900 font-bold">Read More</span>
                     </a>
                 </div>
             </div>
 
-            <div className="flex justify-between items-center mb-6 shadow-2xl shadow-black/50 rounded-2xl overflow-hidden">
-                <div>
-                    
-                    <iframe width="400" height="400" src="https://www.youtube.com/embed/zpAMpxGJCoc?si=_yDhIk3lRtmorh4H" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+            <div className="flex justify-between items-center mb-6 shadow-2xl shadow-black/70 rounded-2xl overflow-hidden">
+                <div className="flex-1">
+                    <iframe width="600" height="360"  src="https://www.youtube.com/embed/zpAMpxGJCoc?si=_yDhIk3lRtmorh4H" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
                 </div>
-                <div className="flex flex-col items-center">
+                <div className="justify-items-center flex-grow flex-1">
                     <h1 className="text-white m-2 font-semibold text-2xl text-center mb-3">TEDxGVSU</h1>
-                    <p className="leading-relaxed text-lg text-white ml-4 mr-6 max-w-120">Younger me talks about a national systemic food waste problem through the lens of dumpster diving!</p>
+                    <p className="leading-relaxed text-lg text-white ml-4 mr-6 max-w-150">Younger me talks about a national systemic food waste problem through the lens of dumpster diving!</p>
                 </div>
                 
             </div>
--- personal-website/src/pages/projects.tsx
+++ personal-website/src/pages/projects.tsx
@@ -10,32 +10,32 @@ export default function Projects() {
     return (
         <>
             <div className="pt-16 flex flex-col justify-center mb-16">
-                <div className="bg-gradient-to-r from-lime-900 to-lime-800   w-full flex flex-col items-center rounded-2xl p-8">
-                    <p className="text-white text-3xl text-shadow-lg mb-3">Lown Index Health Equity Analysis</p>
+                <div className="bg-neutral-800 shadow-2xl shadow-black/100   w-full flex flex-col items-center rounded-2xl p-8">
+                    <p className="text-white text-3xl font-bold text-shadow-lg mb-5">Lown Index Health Equity Analysis</p>
                     <div className="flex flex-wrap mb-3 w-fit rounded p-0.5">
                         <p className="bg-white shadow font-bold text-lime-800 text-lg rounded p-0.5 mr-2 ml-2 bounce-once">Python</p>
                         <p className="bg-white shadow font-bold text-lime-800 text-lg rounded p-0.5 mr-2 ml-2 bounce-once">Pandas</p>
                         <p className="bg-white  shadow-black-30 font-bold text-lime-800 text-lg rounded p-0.5 mr-2 ml-2 bounce-once">SQL</p>
 
                     </div>
-                    <iframe className={`rounded-xl w-full ${isOpen ? "h-300" : "h-75"} mb-4 shadow-lg shadow-black/40`} src="lown_index.html"></iframe>
-                    <button className="text-lime-800 bg-white border-1  py-1 px-2 rounded-xl hover:bg-neutral-200 hover:border-neutral-800 focus:outline-2  focus:outline-lime-700 shadow-black/20 shadow" onClick={showMore}>{isOpen ? "See less" : "See more"}</button>
+                    <iframe className={`rounded-xl w-full transition-height duration-300 hover:scale-102 ease-in-out ${isOpen ? "h-300" : "h-75"} mb-4 shadow-lg shadow-black/40`} src="lown_index.html"></iframe>
+                    <button className="text-lime-950 bg-white border-1 py-1 px-2 rounded-xl shadow-lg shadow-black/60 hover:bg-neutral-200 hover:border-neutral-800 focus:outline-2  focus:outline-lime-700" onClick={showMore}>{isOpen ? "See less" : "See more"}</button>
                 </div>
             </div>
             
             <div className="pt-16 flex flex-col justify-center">
-                <div className="bg-gradient-to-r from-lime-900 to-lime-800  w-full flex flex-col items-center rounded-2xl p-8">
+                <div className="bg-neutral-800 shadow-2xl shadow-black/100 w-full flex flex-col items-center rounded-2xl p-8">
                     <p className="text-white text-3xl text-shadow-lg font-extrabold mb-3">CMS Quality Indicator Database</p>
                     <div className="flex flex-wrap mb-4 w-fit rounded p-0.5">
                         <p className="bg-white shadow font-bold text-lime-800 text-lg rounded p-0.5 mr-2 ml-2 bounce-once">Python</p>
                         <p className="bg-white shadow font-bold text-lime-800 text-lg rounded p-0.5 mr-2 ml-2 bounce-once">Pandas</p>
                         <p className="bg-white  shadow-black-30 font-bold text-lime-800 text-lg rounded p-0.5 mr-2 ml-2 bounce-once">SQL</p>
                     </div>
-                    <div className="flex w-full">
-                        <img src="/cms.png" className="h-45 rounded-lg shadow-xl shadow-black/20"></img>
-                        <p className="m-4 p-4 text-white leading-relaxed text-lg">Using MySQL, an analysis of the Centers for Medicare and Medicaid Value-Based Programs was performed. Although there are many VBPs, the Efficiency and Cost Reduction, Hospital Readmission Reduction, and Disproportionate Share Hospital Programs were selected for this analysis, as insights generate from them could show potential relationships between financial and readmission statistics.</p>
+                    <div className="flex w-full flex-wrap justify-center align-middle">
+                        <img src="/cms.png" className="max-h-100 rounded-lg shadow-xl shadow-black/70"></img>
+                        <p className="m-4 p-4 text-white leading-relaxed text-lg max-w-md flex-1">Using MySQL, an analysis of the Centers for Medicare and Medicaid Value-Based Programs was performed. Although there are many VBPs, the Efficiency and Cost Reduction, Hospital Readmission Reduction, and Disproportionate Share Hospital Programs were selected for this analysis, as insights generate from them could show potential relationships between financial and readmission statistics.</p>
                     </div>
-            </div>
+                </div>
             </div>
         </>
     
